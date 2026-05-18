21e25e86f15f45df6aea466bee54c006b96e8031
kroker9712-dot <kroker9712@gmail.com>
2026-05-18 13:23:59 +0630

    Update run.py
    Fix run.py

--- backend/run.py
+++ backend/run.py
@@ -9,4 +9,6 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    # Render ပေါ်မှာ Port နံပါတ်ကို အော်တိုဖတ်ခိုင်းပြီး Host ကို 0.0.0.0 ပြောင်းပေးခြင်း
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
